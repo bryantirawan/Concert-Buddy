d631d674d4c7ac52160d493b57dd01735b4384bb
bryantirawan <bryantirawan@gmail.com>
2022-07-12 23:48:39 -0700

    about to create poller from buddy to ticket for User

--- ticket-api/ticket_rest/admin.py
+++ ticket-api/ticket_rest/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ConcertVO
+from .models import ConcertVO, UserVO
 
 # Register your models here.
 admin.site.register(ConcertVO)
+admin.site.register(UserVO)
