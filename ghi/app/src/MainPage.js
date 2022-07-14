5d0b860c1a504bdae130771d0cefd041f3f776da
Justin Pang <jpang1004@gmail.com>
2022-07-14 17:21:45 -0400

    fixed react app

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -7,7 +7,7 @@ function MainPage() {
       <h1 className="display-5 fw-bold">Concert Buddy</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          The premiere solution for aconcert buddy management 
+          The premiere solution for concert buddy management 
         </p>
       </div>
     </div>
