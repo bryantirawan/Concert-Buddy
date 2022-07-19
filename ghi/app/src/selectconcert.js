5da50eb3d33f98c2d41c75949f77ff43ef489828
Ben <benjamin.perlman@gmail.com>
2022-07-18 18:02:26 -0700

    Fixed Artist Search not showing old results

--- ghi/app/src/selectconcert.js
+++ ghi/app/src/selectconcert.js
@@ -17,6 +17,9 @@ export default function Concerts() {
 
     const current = new Date();
     const date = `${current.getDate()}-${('0' + (current.getMonth()+1)).slice(-2)}-${current.getFullYear()}`;
+    const day = `${current.getDate()}`;
+    const month = `${('0' + (current.getMonth()+1)).slice(-2)}`;
+
 
 
     const handleSubmit = (e) => {
@@ -30,9 +33,33 @@ export default function Concerts() {
 
             const fetchConcert = async () => {
                 const concertResponse = await fetch(`http://localhost:8080/api/selectconcertsforcity/${final_city}/&p=1`);
+                
                 const concertData = await concertResponse.json() 
                 console.log('setlist', concertData.concerts.setlist)
-                setConcerts(concertData.concerts.setlist) 
+                if (concertData.concerts.setlist) {
+                    for (let i in concertData.concerts.setlist){
+                        const dateParts = concertData.concerts.setlist[i].eventDate.split("-");
+                        const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+                        concertData.concerts.setlist[i].eventDate = dateObject
+
+                    } 
+                    setConcerts(concertData.concerts.setlist) 
+
+                }
+                    // const concertResponse = await fetch(`http://localhost:8080/api/selectconcertsforcity/Chicago/&p=1`);
+                
+                    // const concertData = await concertResponse.json() 
+
+                    //  console.log(concertData.artist)
+                    // // concertData.artist.name = "errpr"
+                    // // concertData.eventDate = date
+                    // console.log('setlist', concertData.concerts.setlist)
+
+                    // setConcerts(concertData.concerts.setlist) 
+
+                    
+               
+                
             }
             fetchConcert()
 
@@ -50,13 +77,33 @@ export default function Concerts() {
                 const concertResponse = await fetch(`http://localhost:8080/api/concerts/artist/${final_artist}/`);
                 const concertData = await concertResponse.json() 
                 console.log('setlist', concertData.concerts.setlist)
+             
+                // var dateParts = concertData.concerts.setlist[0].eventDate.split("-");
+
+                // var dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+                // console.log(dateObject)
+                if (concertData.concerts.setlist) {
+                    for (let i in concertData.concerts.setlist){
+                        const dateParts = concertData.concerts.setlist[i].eventDate.split("-");
+                        const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+                        concertData.concerts.setlist[i].eventDate = dateObject
+
+                    } 
+
                 setConcerts(concertData.concerts.setlist) 
             }
+        }
             fetchConcert()
 
     }
     
+//     const dates = licitacoes
+//     .filter((bigN) => new Date(bigN.licitacoesDate) < currentDate)
+//     .map((newNumber) => newNumber.licitacoesDate);
+//   console.log(dates)
 
+
+  //new Date(concert.eventDate) >= date
     return (
         <>
         <div>
@@ -83,11 +130,13 @@ export default function Concerts() {
         </tr>
     </thead>
         <tbody>
-        {concerts.filter(concert => date<=concert.eventDate).map((concert,idx) => (
+        {/* {concerts.filter(concert => date<=concert.eventDate).map((concert,idx) => ( */}
+        {concerts.filter(concert => ((concert.eventDate)) >= (Date.now())).map((concert,idx) => (
+//Date.now()
                 <tr key={idx}>
                     <td>{concert.artist.name}</td>  
                     <td>{concert.venue.name}</td>
-                    <td>{concert.eventDate}</td>
+                    <td>{concert.eventDate.toLocaleDateString()} </td> 
                     <td>
                         <form action={`http://localhost:8100/api/add/${concert.id}/`} method="POST">
                         <button>        
@@ -100,6 +149,36 @@ export default function Concerts() {
             }  
         </tbody>
     </table>
+    {/* <table>
+    <thead>
+        <tr>
+            <th>Artist</th>
+            <th>Venue</th>
+            <th>Date</th>
+            <th>Save Concert</th>
+        </tr>
+    </thead>
+        <tbody>
+            
+        {/* {concerts.filter(concert => date<=concert.eventDate).map((concert,idx) => ( */}
+        {/* {concerts.filter(concert => date).map((concert,idx) => (
+
+                <tr key={idx}>
+                    <td>{concert.artist.name}</td>  
+                    <td>{concert.venue.name} x { Date.parse(concert.eventDate)}</td>
+                    <td>{concert.eventDate}</td>
+                    <td>
+                        <form action={`http://localhost:8100/api/add/${concert.id}/`} method="POST">
+                        <button>        
+                        Save concert to concert model in concert microservice
+                        </button>
+                        </form> 
+                    </td>
+                </tr>
+            )) 
+            }  
+        </tbody>
+    </table> */} 
         </div>
 
         </>
