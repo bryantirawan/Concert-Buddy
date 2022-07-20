b5bc1c5e092022076bce60aba0a9758957c7c0d0
Erica <ericavdippold@gmail.com>
2022-07-19 21:18:54 -0400

    Changes to select concerns page

--- ghi/app/src/selectconcert.js
+++ ghi/app/src/selectconcert.js
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from 'react'; 
+import React, {useEffect, useState} from 'react';
+import Toggle from './components/Toggle';
 
 export default function Concerts() {
     const [concerts, setConcerts] = useState([]);
     const [city, setCity] = useState('');
     const [artist, setArtist] = useState('');
+    const [toggled, setToggled] = useState(false);
 
     useEffect( () => {
         const fetchConcert = async () => {
-            const concertResponse = await fetch(`http://localhost:8080/api/selectconcerts`); 
+            const concertResponse = await fetch(`http://localhost:8080/api/selectconcerts`);
             const concertData = await concertResponse.json();
             setConcerts(concertData.setlist);
         }
@@ -21,15 +23,15 @@ export default function Concerts() {
 
     const handleLocationSubmit = (e) => {
         e.preventDefault();
-        const city_new = city.split(' ') 
-        let final_city = city_new[0] 
+        const city_new = city.split(' ')
+        let final_city = city_new[0]
         for (let i = 1; i < city_new.length; i++) {
             final_city += '%20'
             final_city += city_new[i]
         }
         fetch(`http://localhost:8080/api/selectconcertsforcity/${final_city}/&p=1`).then((concertResponse) => {
             if(concertResponse.ok) {
-                return concertResponse.json();          
+                return concertResponse.json();
             }
             throw new Error('Invalid Search Request');
         })
@@ -40,9 +42,9 @@ export default function Concerts() {
                     const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
                     concertData.concerts.setlist[i].eventDate = dateObject
 
-                } 
-                setConcerts(concertData.concerts.setlist); 
-            } 
+                }
+                setConcerts(concertData.concerts.setlist);
+            }
             setArtist('');
         })
         .catch((error) => {
@@ -59,8 +61,8 @@ export default function Concerts() {
     }
     const handleArtistSubmit = (e) => {
         e.preventDefault();
-        const artist_new = artist.split(' ') 
-        let final_artist = artist_new[0] 
+        const artist_new = artist.split(' ')
+        let final_artist = artist_new[0]
         for (let i = 1; i < artist_new.length; i++) {
             final_artist += '%20'
             final_artist += artist_new[i]
@@ -78,9 +80,9 @@ export default function Concerts() {
                     const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
                     concertData.concerts.setlist[i].eventDate = dateObject
 
-                } 
-                setConcerts(concertData.concerts.setlist); 
-            } 
+                }
+                setConcerts(concertData.concerts.setlist);
+            }
             setCity('');
         })
         .catch((error) => {
@@ -88,63 +90,87 @@ export default function Concerts() {
             setConcerts(undefined);
         });
 
+
+
+
             // const fetchConcert = async () => {
             //     const concertResponse = await fetch(`http://localhost:8090/api/concerts/artist/${final_artist}/`);
-            //     const concertData = await concertResponse.json() 
+            //     const concertData = await concertResponse.json()
             //     console.log(concertData)
-            //     setConcerts(concertData.concerts.setlist) 
+            //     setConcerts(concertData.concerts.setlist)
             // }
             // fetchConcert()
 
     }
-    
+
+    const handleKeypress = e => {
+        //it triggers by pressing the enter key
+      if (e.keyCode === 13) {
+        if (toggled == false) {
+            handleArtistSubmit()
+        } else {
+        handleLocationSubmit();
+        }
+      }
+    };
+
     return (
         <>
-        <div>
-            <p>
-            Hi
-            </p>
+        <div className='selectconcerts'>
+            <Toggle onChange={(e) => setToggled(e.target.checked)} />
+            <p>  Search by {toggled ? "City ": "Artist "}</p>
+            <div className='entry'>
+                { toggled ?
             <form onSubmit={handleLocationSubmit}>
-                <label>City:  </label>
-                <input type="text" value={city} required onChange={(e) => {setCity(e.target.value)}} />
-                <input type="submit" value="Fetch concerts for city"/>
+                {/* <label>City:  </label> */}
+                <input type="text" value={city} required onChange={(e) => {setCity(e.target.value)}} onKeyPress={handleKeypress}/>
+                {/* <input type="submit" value="Fetch concerts for city"/> */}
             </form>
+            :
             <form onSubmit={handleArtistSubmit}>
-                <label>Artist:  </label>
-                <input type="text" value={artist} required onChange={(e) => {setArtist(e.target.value)}} />
-                <input type="submit" value="Fetch concerts by artist"/>
+                {/* <label>Artist:  </label> */}
+                <input type="text" value={artist} required onChange={(e) => {setArtist(e.target.value)}} onKeyPress={handleKeypress}/>
+                {/* <input type="submit" value="Fetch concerts by artist"/> */}
             </form>
+            }
         </div>
+        <p></p>
+        <div>
     {concerts !== undefined ?
-    (<table>
+    (
+    <table>
     <thead>
+
+        <br></br>
         <tr>
-            <th>Artist</th>
+            {/* <th>Artist</th>
             <th>Venue</th>
             <th>Date</th>
-            <th>Save Concert</th>
+            <th>Save Concert</th> */}
         </tr>
     </thead>
         <tbody>
         {concerts.filter(concert => ((concert.eventDate)) >= (Date.now())).map((concert,idx) => (
                 <tr key={idx}>
-                    <td>{concert.artist.name}</td>  
+                    <td>{concert.artist.name}</td>
                     <td>{concert.venue.name}</td>
-                    <td>{concert.eventDate.toLocaleDateString()} </td> 
+                    <td>{concert.eventDate.toLocaleDateString()} </td>
                     <td>
                         <form action={`http://localhost:8100/api/add/${concert.id}/`} method="POST">
-                        <button>        
+                        <button>
                         Save concert to concert model in concert microservice
                         </button>
-                        </form> 
+                        </form>
                     </td>
                 </tr>
-            )) 
-        }  
+            ))
+        }
         </tbody>
-    </table>) : 
+    </table>) :
     (<p>Invalid Search Request</p>)
     }
+    </div>
+    </div>
     </>
     )
 }
