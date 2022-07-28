8ab3e8be78e5e6aa63bfe030d5c68b2e7526947d
bryantirawan <bryantirawan@gmail.com>
2022-07-28 15:31:21 -0700

    made login page prettier

--- ghi/app/src/pages/LoginPage.js
+++ ghi/app/src/pages/LoginPage.js
@@ -1,53 +1,50 @@
 import { React, useContext } from "react"
 import AuthContext from "../context/AuthContext"
-// import "./signup.css";
 import { useNavigate } from 'react-router-dom';
 
 
 const LoginPage = () => {
-
   let {loginUser} = useContext(AuthContext)
   let navigate = useNavigate()
 
-
   return (
-
-
 <>
-    
-    <div>
-
-      <form class="form-signin" onSubmit={loginUser}>
-
-        
-        <label for="username" class="sr-only">Email address</label>
-
-        <input type="text" id="username" class="form-control" name="username" placeholder='Enter Username' />
-        <input type="password" name="password" class="form-control" placeholder='Enter Password' />
-        {/* <input type="submit" />  */}
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-
-      </form>
-    </div>
-    <div>
-    <h1> Don't have an account? </h1>
-        {/* <form action='/signup'>
-            <button>Sign up</button>
-        </form> */}
+<div className="my-5 container">
+<form class="form-signin" onSubmit={loginUser}>
+        <div className="row">
+          <div className="col">
+            <div className="card shadow">
+              <div className="card-body">
+                  <h1 className="card-title">Login</h1>
+                  <div className="row">
+                    <div className="col">
+                      <div className="form-floating mb-3">
+                      <input type="text" id="username" class="form-control" name="username" placeholder='Enter Username' />
+                        <label htmlFor="username">Username</label>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <div className="form-floating mb-3">
+                        <input type="password" name="password" class="form-control" placeholder='Enter Password' />
+                          <label htmlFor="password1">Password</label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button className="btn btn-lg btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <h3> Don't have an account? </h3>
         <button className="btn btn-primary btn-lg " onClick={(e) => navigate('/sign-up')} >Register </button> 
-
-    </div>
-
-
-
-
-
-
-    
+  </form>
+      </div>
     </>
   )
 }
-
 export default LoginPage
 
 
