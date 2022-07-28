79f2f573370d9620c3bbf7c64b1c67804a36031f
bryantirawan <bryantirawan@gmail.com>
2022-07-28 10:37:01 -0700

    css work

--- ghi/app/src/components/selectconcert.js
+++ ghi/app/src/components/selectconcert.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import Toggle from './Toggle';
 import { useContext } from 'react'
 import AuthContext from '../context/AuthContext';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 
