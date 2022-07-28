7973c732a56114c0b23a6c74ef1952fc6bac9ade
Erica <ericavdippold@gmail.com>
2022-07-27 22:35:03 -0400

    progress on buyer and seller personal ticket pages

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -12,6 +12,7 @@ import SearchToSellTickets from './components/pages/UselessSearchSellTickets';
 import Fellowusersgoingtoconcert from './components/fellowusersgoingtoconcert';
 import TicketCheckout from './components/pages/TicketCheckout';
 import SellerTicketList from './components/pages/SellerPersonalTickets';
+import BuyerConcerts from './components/pages/BuyerPersonalTickets';
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
           <Route path="/selltickets" element={<SearchToSellTickets />} />
           <Route path="/checkout/:ticket_id" element={<TicketCheckout />} />
           <Route path="/sellertickets" element={<SellerTicketList />} />
+          <Route path="/myconcerts" element={<BuyerConcerts />} />
         </Routes>
         </div>
         </AuthProvider>
--- ghi/app/src/components/pages/SellerPersonalTickets.js
+++ ghi/app/src/components/pages/SellerPersonalTickets.js
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../context/AuthContext';
 import { useContext } from 'react'
 
-
-
 function TicketColumn() {
 
 const [unsold_tickets, setUnsoldTickets] = useState([]);
@@ -17,23 +15,27 @@ useEffect( () => {
     const fetchTickets = async() => {
         const ticketResponse = await fetch(`http://localhost:8090/api/tickets/`);
         const ticketData = await ticketResponse.json();
-        console.log(ticketData)
-        // for (let ticket in ticketData) {
-        //     console.log(ticket.seller)
-        // }
-
-        // for (let tick of ticketData) {
-        //     if (seller === ticketData.seller) {
-        //         console.log(tick)
-        //     }
-        // }
-        // setTickets[ticketData];
-
+        let unsold_list = []
+        let sold_list = []
+        for (let ticket of ticketData.tickets) {
+            console.log(ticket)
+            let person = ticket.seller.import_href.slice(11)
+            if (person == seller && ticket.sold == false) {
+                unsold_list.push(ticket)
+            } else if (ticket.seller.id == seller && ticket.sold == true) {
+                sold_list.push(ticket)
+            } else {
+                return "ERROR WITH FUNCTIONALITY"
+            }
+        }
+        console.log(sold_list, "sold list")
+        console.log(unsold_list, "unsold_list")
+        setUnsoldTickets(unsold_list)
+        setSoldTickets(sold_list)
     }
     fetchTickets()
 }, []
 );
-
     return (
       <div className="col">
         {/* {props.list.map(data => {
@@ -68,13 +70,13 @@ function SellerTicketList() {
 
     return (
         <>
-        <div className="my-4 container">
+        <div className="my-4 container bg-dark">
         <div className="px-4 py-4 my-4 mt-0 text-center bg-secondary">
           <img className="bg-white rounded shadow d-block mx-auto mb-1" alt="" width="600" />
           <h1 className="display-6 fw-bold">Ticket Listings</h1>
           <div className="col-lg-6 mx-auto">
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <Link to="/selectconcerts" className="btn btn-dark btn-lg px-4 gap-3">Sell another ticket</Link>
+              <Link to="/selectconcerts" className="btn btn-dark btn-lg px-4 gap-3">Sell new ticket</Link>
             </div>
           </div>
         </div>
