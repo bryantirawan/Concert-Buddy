88f7cde3d5ac09f40b00b8f7dbcec5ce50502193
bryantirawan <bryantirawan@gmail.com>
2022-07-28 14:27:33 -0700

    cleaned table

--- ghi/app/src/components/selectconcert.js
+++ ghi/app/src/components/selectconcert.js
@@ -170,8 +170,8 @@ export default function Concerts() {
 
             <th>Venue</th>
             <th>Date</th>
-            {user ? (<th>Wanna go?</th>) : (<th>Want to find a buddy? Log in first</th>)}
-            {user ? (<th>Have a ticket to sell?</th>) : (<th>Log in first to sell a ticket</th>)}
+            {user ? (<th>Wanna go?</th>) : (<th>Login to find buddies</th>)}
+            {user ? (<th>Have a ticket to sell?</th>) : (<th>Login to sell tickets</th>)}
 
 
 
@@ -191,14 +191,13 @@ export default function Concerts() {
                         </button>
                     </form>
                     </td>) : (<td></td>)}
-
-                    <td>
-                    <form onSubmit={(e) => handleAddConcertSubmit(e, concert.venue.name, concert.venue.city.name, concert.eventDate, concert.artist.name, concert.id, concert.venue.id, concert.artist.mbid)}>
+                    {user ? (<form onSubmit={(e) => handleAddConcertSubmit(e, concert.venue.name, concert.venue.city.name, concert.eventDate, concert.artist.name, concert.id, concert.venue.id, concert.artist.mbid)}>
                         <button type="submit">
                         Sell ticket
                         </button>
-                    </form>
-                    </td>
+                    </form>):(<td></td>)}
+
+                 
                 </tr>
             ))
         }
