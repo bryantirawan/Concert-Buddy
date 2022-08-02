33527ce59db1c9cfd8f1a43d1d6a0d6c11404c3f
bryantirawan <bryantirawan@gmail.com>
2022-08-02 12:05:18 -0700

    fixed class to className

--- ghi/app/src/components/Footer.js
+++ ghi/app/src/components/Footer.js
@@ -10,13 +10,13 @@ function Footer() {
     <div className='footer-container'>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h3>Tickets</h3>
             <Link to='/selectconcerts'>Sell a ticket</Link>
             <Link to='/selectconcerts'>Buy a ticket</Link>
             <Link to='/selectconcerts' reloadDocument>Find a Buddy</Link>
           </div>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h3>User profile</h3>
             <Link to='/mytickets'>Your Tickets</Link>
             <Link to='/sellertickets'>Tickets you're selling</Link>
@@ -24,13 +24,13 @@ function Footer() {
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h3>Support</h3>
             <Link to='/'>Contact</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
+          <div className='footer-link-items'>
             <h3>About us</h3>
             <Link to='/'>About</Link>
             <Link to='/'>Jobs</Link>
@@ -40,41 +40,41 @@ function Footer() {
         </div>
       </div>
       <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='footer-logo'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
             <Link to='/' className='social-logo'>
               Concert Buddy
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>Concert Buddy © 2022</small>
-          <div class='social-icons'>
+          <small className='website-rights'>Concert Buddy © 2022</small>
+          <div className='social-icons'>
             <Link
-              class='social-icon-link facebook'
+              className='social-icon-link facebook'
               to='/'
               target='_blank'
               aria-label='Facebook'
             >
-              <i class='fab fa-facebook-f' />
+              <i className='fab fa-facebook-f' />
             </Link>
             <Link
-              class='social-icon-link instagram'
+              className='social-icon-link instagram'
               to='/'
               target='_blank'
               aria-label='Instagram'
             >
-              <i class='fab fa-instagram' />
+              <i className='fab fa-instagram' />
             </Link>
             <Link
-              class='social-icon-link youtube'
+              className='social-icon-link youtube'
               to='/'
               target='_blank'
               aria-label='Youtube'
             >
-              <i class='fab fa-youtube' />
+              <i className='fab fa-youtube' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              className='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='Twitter'
@@ -82,12 +82,12 @@ function Footer() {
               <i class='fab fa-twitter' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              className='social-icon-link twitter'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
             >
-              <i class='fab fa-linkedin' />
+              <i className='fab fa-linkedin' />
             </Link>
           </div>
         </div>
