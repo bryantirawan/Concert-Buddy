bffccee80c9f7c4a21862c70efda332e20d8d818
bryantirawan <bryantirawan@gmail.com>
2022-08-03 16:44:53 -0700

    cleaned

--- ghi/app/src/components/ticketpages/TicketCheckout.js
+++ ghi/app/src/components/ticketpages/TicketCheckout.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react'
 import AuthContext from '../../context/AuthContext';
 import {
