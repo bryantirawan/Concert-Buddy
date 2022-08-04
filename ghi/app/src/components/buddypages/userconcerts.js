e82d249d4db0ee7d7a676833571c5f5c73f3b973
Erica <ericavdippold@gmail.com>
2022-08-04 14:45:43 -0400

    trying changes w pollers and dependencies

--- ghi/app/src/components/buddypages/userconcerts.js
+++ ghi/app/src/components/buddypages/userconcerts.js
@@ -32,7 +32,7 @@ export default function Userconcerts() {
             }
         }
         getUserConcerts();
-    }
+    }, [authTokens, logoutUser]
     )
 
     const putConcertandputUser = async (concID) => {
