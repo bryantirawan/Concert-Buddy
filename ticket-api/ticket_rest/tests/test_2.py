8c9fd4dff7ba667aeeccd47786d1ac3cd1e45c6f
Erica <ericavdippold@gmail.com>
2022-08-14 16:39:38 -0400

    testing new test

--- ticket-api/ticket_rest/tests/test_2.py
+++ ticket-api/ticket_rest/tests/test_2.py
@@ -1,9 +1,9 @@
 from django.test import TestCase, Client
 from django_fake_model import models as f
 from django.db import models
-from ..models import ConcertVO, UserVO
+# from ..models import ConcertVO, UserVO
 import json
-import datetime
+# import datetime
 
 
 class FakeConcertVOModel(f.FakeModel):
@@ -26,50 +26,50 @@ class ApiTicketsTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("tickets", content)
 
-    @FakeConcertVOModel.fake_me
-    def test_post_tickets(self):
-        FakeConcertVOModel.objects.create(
-            artist="test_artist",
-            city="test_city",
-            concert_id="53b3eb72",
-            date=datetime.date.today(),
-            venue="test_venue",
-            )
-        model = FakeConcertVOModel.objects.get(artist="test_artist")
-        self.assertEqual(model.artist, "test_artist")
+    # @FakeConcertVOModel.fake_me
+    # def test_post_tickets(self):
+    #     FakeConcertVOModel.objects.create(
+    #         artist="test_artist",
+    #         city="test_city",
+    #         concert_id="53b3eb72",
+    #         date=datetime.date.today(),
+    #         venue="test_venue",
+    #         )
+    #     model = FakeConcertVOModel.objects.get(artist="test_artist")
+    #     self.assertEqual(model.artist, "test_artist")
 
-        ConcertVO.objects.create(
-            artist="test_artist",
-            city="test_city",
-            concert_id="53b3eb74",
-            date=datetime.date.today(),
-            venue="test_venue",
-            )
+    #     ConcertVO.objects.create(
+    #         artist="test_artist",
+    #         city="test_city",
+    #         concert_id="53b3eb74",
+    #         date=datetime.date.today(),
+    #         venue="test_venue",
+    #         )
 
-        UserVO.objects.create(
-            first_name="test_first",
-            last_name="test_last",
-            email="test@test.com"
-            )
+    #     UserVO.objects.create(
+    #         first_name="test_first",
+    #         last_name="test_last",
+    #         email="test@test.com"
+    #         )
 
-        ticket = json.dumps({
-            "price": 200,
-            "section": "A24",
-            "row": "17",
-            "seat": "22",
-            "picture_url": "ticket.jpeg",
-            "concert": "53b3eb74",
-            "seller": 1,
-            "buyer": None
-        })
-        response = self.client.post("/api/tickets/", ticket, "application/json")
-        content = json.loads(response.content)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("price", content)
-        self.assertIn("section", content)
-        self.assertIn("row", content)
-        self.assertIn("seat", content)
-        self.assertIn("picture_url", content)
-        self.assertIn("concert", content)
-        self.assertIn("seller", content)
-        self.assertIn("buyer", content)
+    #     ticket = json.dumps({
+    #         "price": 200,
+    #         "section": "A24",
+    #         "row": "17",
+    #         "seat": "22",
+    #         "picture_url": "ticket.jpeg",
+    #         "concert": "53b3eb74",
+    #         "seller": 1,
+    #         "buyer": None
+    #     })
+    #     response = self.client.post("/api/tickets/", ticket, "application/json")
+    #     content = json.loads(response.content)
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertIn("price", content)
+    #     self.assertIn("section", content)
+    #     self.assertIn("row", content)
+    #     self.assertIn("seat", content)
+    #     self.assertIn("picture_url", content)
+    #     self.assertIn("concert", content)
+    #     self.assertIn("seller", content)
+    #     self.assertIn("buyer", content)
